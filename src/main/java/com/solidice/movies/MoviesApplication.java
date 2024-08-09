9cc7e9c67e02394e597b54307211837d44c39bb4
Shubham <sk192196@gmail.com>
2024-08-09 19:28:32 +0530

    Initial commit

---
+++ src/main/java/com/solidice/movies/MoviesApplication.java
@@ -0,0 +1,29 @@
+package com.solidice.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@SpringBootApplication
+
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer configure(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry reg){
+				reg.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
+
+
+}
