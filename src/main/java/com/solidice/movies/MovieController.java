5703d4592a6a821b57e9f54c5ecc9dc7e8813cc6
Shubham Pathak <95904081+Shubhh4@users.noreply.github.com>
2026-09-06 11:56:48 +0530

    Fix import statement for ObjectId in MovieController

--- src/main/java/com/solidice/movies/MovieController.java
+++ src/main/java/com/solidice/movies/MovieController.java
@@ -1,6 +1,6 @@
 package com.solidice.movies;
 
-import org.bson.types.ObjectId;;;;;;
+import org.bson.types.ObjectId;;;;;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
